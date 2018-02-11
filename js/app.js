e790137a2ab4c968b080db67b229f9b0581e72a3
LizbethFP <lizbeth.fp@gmail.com>
2018-02-11 13:48:40 -0500

    subiendo plug-in cardify y transition para efecto de imgs

--- js/app.js
+++ js/app.js
@@ -1,4 +1,12 @@
-function begin() {
+$(window).on('load', function() {
+  $('img').cardify();
 
-}
-$(document).ready(begin);
+  /* EFECTO DE GIRAR CERTIFICADOS AL PASAR MOUSE */
+  $('.fa-certificate').on('mouseover', function() {
+  $(this).transition({ rotate: '10deg' });
+  });
+  /* EFECTO DE GIRAR CERTIFICADOS A SU POSICIÓN DE ORGINE AL SACAR EL MOUSE */
+  $('.fa-certificate').on('mouseleave', function() {
+  $(this).transition({ rotate: '-10deg' });
+  });
+});
