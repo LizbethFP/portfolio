1aace65cf2b7da2ba94f27459b2a91c8849d4f9b
LizbethFP <lizbeth.fp@gmail.com>
2018-03-19 16:20:58 -0500

    añadiendo header fixed, mejorando vista mobile, agrandado fuente en contacto

--- js/app.js
+++ js/app.js
@@ -3,11 +3,27 @@ $(window).on('load', function() {
 
   /* EFECTO DE GIRAR CERTIFICADOS AL PASAR MOUSE */
   $('.fa-certificate').on('mouseover', function() {
-  $(this).transition({ rotate: '10deg' });
+    $(this).transition({
+      rotate: '10deg'
+    });
   });
-  
+
   /* EFECTO DE GIRAR CERTIFICADOS A SU POSICIÓN DE ORGINE AL SACAR EL MOUSE */
   $('.fa-certificate').on('mouseleave', function() {
-  $(this).transition({ rotate: '-10deg' });
+    $(this).transition({
+      rotate: '-10deg'
+    });
   });
+
+  // var menuOffset = $('#header').offset();
+  // $(document).on('scroll', function() {
+  //   var scrollTop = $(document).scrollTop();
+  //   if (scrollTop > menuOffset.top) {
+  //     $('#menu-nav').addClass('menu-white');
+  //     $('#menu-nav').removeClass('menu-transparent');
+  //   } else {
+  //     $('#menu-nav').removeClass('menu-white');
+  //     $('#menu-nav').addClass('menu-transparent');
+  //   }
+  // });
 });
