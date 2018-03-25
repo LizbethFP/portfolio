94f1e0c12765527f85642b5d504cd278b0de3749
LizbethFP <lizbeth.fp@gmail.com>
2018-03-25 16:21:18 -0500

    magicscroll init

--- js/app.js
+++ js/app.js
@@ -1,29 +1,24 @@
 $(window).on('load', function() {
   $('img').cardify();
 
-  /* EFECTO DE GIRAR CERTIFICADOS AL PASAR MOUSE */
-  $('.fa-certificate').on('mouseover', function() {
-    $(this).transition({
-      rotate: '10deg'
-    });
-  });
-
-  /* EFECTO DE GIRAR CERTIFICADOS A SU POSICIÓN DE ORGINE AL SACAR EL MOUSE */
-  $('.fa-certificate').on('mouseleave', function() {
-    $(this).transition({
-      rotate: '-10deg'
-    });
-  });
-
-  // var menuOffset = $('#header').offset();
-  // $(document).on('scroll', function() {
-  //   var scrollTop = $(document).scrollTop();
-  //   if (scrollTop > menuOffset.top) {
-  //     $('#menu-nav').addClass('menu-white');
-  //     $('#menu-nav').removeClass('menu-transparent');
-  //   } else {
-  //     $('#menu-nav').removeClass('menu-white');
-  //     $('#menu-nav').addClass('menu-transparent');
-  //   }
+  // /* EFECTO DE GIRAR CERTIFICADOS AL PASAR MOUSE */
+  // $('.fa-certificate').on('mouseover', function () {
+  //   $(this).transition({
+  //     rotate: '10deg'
+  //   });
   // });
+
+  // /* EFECTO DE GIRAR CERTIFICADOS A SU POSICIÓN DE ORGINE AL SACAR EL MOUSE */
+  // $('.fa-certificate').on('mouseleave', function () {
+  //   $(this).transition({
+  //     rotate: '-10deg'
+  //   });
+  // });
+
+  var controller = new ScrollMagic();
+  var ourScene = new ScrollMagic.Scene({
+    triggerElement: '#about-me'
+  })
+    .setClassToggle('#about-me', 'fade-in')
+    .addTo(controller);
 });
