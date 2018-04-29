8a044c32a8078f9f77cba05334d05905542f8fe5
LizbethFP <lizbeth.fp@gmail.com>
2018-04-28 20:16:47 -0500

    adds active menu acc to scroll

--- js/app.js
+++ js/app.js
@@ -1,24 +1,25 @@
 $(window).on('load', function() {
   $('img').cardify();
+  /* ACTIVE MENU ACCORDING TO SCROLL */
+  var section1OffsetTop = $('.section1').offset().top;
+  var section2OffsetTop = $('.section2').offset().top;
+  var section3OffsetTop = $('.section3').offset().top;
+  var section4OffsetTop = $('.section4').offset().top;
 
-  // /* EFECTO DE GIRAR CERTIFICADOS AL PASAR MOUSE */
-  // $('.fa-certificate').on('mouseover', function () {
-  //   $(this).transition({
-  //     rotate: '10deg'
-  //   });
-  // });
+  $(document).on('scroll', function() {
+    let scrollTop = $(document).scrollTop();
+    var activeLi;
 
-  // /* EFECTO DE GIRAR CERTIFICADOS A SU POSICIÓN DE ORGINE AL SACAR EL MOUSE */
-  // $('.fa-certificate').on('mouseleave', function () {
-  //   $(this).transition({
-  //     rotate: '-10deg'
-  //   });
-  // });
-
-  var controller = new ScrollMagic();
-  var ourScene = new ScrollMagic.Scene({
-    triggerElement: '#about-me'
-  })
-    .setClassToggle('#about-me', 'fade-in')
-    .addTo(controller);
+    if (scrollTop < section2OffsetTop) {
+      activeLi = $('.menu-items>li:nth-child(1)');
+    } else if (scrollTop < section3OffsetTop) {
+      activeLi = $('.menu-items>li:nth-child(2)');
+    } else if (scrollTop < section4OffsetTop) {
+      activeLi = $('.menu-items>li:nth-child(3)');
+    } else {
+      activeLi = $('.menu-items>li:nth-child(4)');
+    }
+    activeLi.addClass('on-section');
+    $('.menu-items>li').not(activeLi).removeClass('on-section');
+  });
 });
